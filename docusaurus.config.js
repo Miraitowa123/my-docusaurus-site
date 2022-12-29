054deb4f2258c4d9e192abe8e8ea7a86dad5e935
huangju <hrx19981122@163.com>
2022-12-29 15:11:30 +0800

    fighting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Kevin domain',
-  tagline: '那年我双手插兜，不知道什么叫对手。',
+  tagline: '一起张开双翼，奔赴星河万里。',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
